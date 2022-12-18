b76b94d3cba2d15631c830d6fc8875f61818b6f5
Ed Grosvenor <ed.grosvenor@wickedviral.com>
2022-12-18 09:36:39 +0100

    WIP

---
+++ resources/views/components/header-buttons.blade.php
@@ -0,0 +1,15 @@
+
+    <a href="mailto:ed@gros.co" class="inline-flex items-center rounded-full
+                            border border-gray-300 bg-white
+                         px-2.5 py-1.5 font-medium text-gray-700 shadow-sm hover:bg-gray-50
+                         focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+        <i class="fa-regular fa-envelope"></i></a>
+
+    <a href="https://phpc.social/@edgrosvenor" class="inline-flex items-center rounded-full
+                            border border-gray-300 bg-white
+                         px-2.5 py-1.5 text-xs font-medium text-gray-700 shadow-sm hover:bg-gray-50
+                         focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Follow</a>
+
+
+
+
